394fe2084799ddc8a1fd7fa5363f6a02948ed2f1
root <root@localhost.localdomain>
2016-06-01 15:04:24 -0400

    new mod

---
+++ modules/execbat.py
@@ -0,0 +1,9 @@
+#execbat.py
+import os
+import subprocess
+def run(**args):
+    path = "Python35-32/testdir/fibo.py"
+    fp = "python %s 60" %path
+    a=subprocess.check_output(fp, stderr=subprocess.STDOUT, shell=True)
+    return(str(a))
+    
